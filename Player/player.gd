58c25dd4a217e6e59c67ab1d7c177a82953cd3ba
ganpaku <fehrlejoni@gmail.com>
2023-12-30 15:42:06 +0100

    first commit

---
+++ Player/player.gd
@@ -0,0 +1,31 @@
+extends RigidBody3D
+@onready var lane_right = $"../LaneRight"
+@onready var lane_markers = $"../LaneMarkers"
+var lanes = [Marker3D]
+var currentLane
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	lanes = lane_markers.get_children()
+	print(lanes)
+	currentLane = 1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_right"):
+		if currentLane >= lanes.size() -1:
+			return
+		else:
+			currentLane += 1
+			move_player(currentLane)
+	if Input.is_action_just_pressed("ui_left"):
+		if currentLane <= 0:
+			return
+		else:
+			currentLane -= 1
+			move_player(currentLane)
+			#global_position = lane_right.global_position
+func move_player(index):
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", lanes[index].global_position, 0.2)
+	
+	
---
+++ World/LanePath.gd
@@ -0,0 +1,12 @@
+extends Path3D
+
+@onready var path_follow_3d = $PathFollow3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	path_follow_3d.progress -= 20 * delta
