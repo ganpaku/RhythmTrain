03b4f8a4fa194d9d2907cdfc0271c46313fd8a41
ganpaku <fehrlejoni@gmail.com>
2024-01-01 15:12:04 +0100

    Imported new playermodel, added animations for hopping lanes and on beat. Fiddled with sky and light.

--- Player/player.gd
+++ Player/player.gd
@@ -3,12 +3,16 @@ extends RigidBody3D
 var lanes = [Marker3D]
 var currentLane
 @export var data : Resource
+@onready var conductor = %Conductor
+@onready var hop_anim = %HopAnim
+@onready var beat_anim = %BeatAnim
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	lanes = lane_markers.get_children()
 	print(lanes)
 	currentLane = 1
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_right"):
@@ -17,15 +21,32 @@ func _process(_delta):
 		else:
 			currentLane += 1
 			move_player(currentLane)
+			hop_anim.play("HopRight")
 	if Input.is_action_just_pressed("ui_left"):
 		if currentLane <= 0:
 			return
 		else:
 			currentLane -= 1
 			move_player(currentLane)
+			hop_anim.play("HopLeft")
 			#global_position = lane_right.global_position
 func move_player(index):
 	var tween = create_tween()
 	tween.tween_property(self, "global_position", lanes[index].global_position, 0.2)
 	
 	
+
+
+func _on_conductor_half_beat():
+	pass
+	
+	#position.y += 4
+
+
+func _on_conductor_eighth_beat():
+	pass
+
+
+func _on_conductor_quarter_beat():
+	beat_anim.play("EveryBeatSquash")
+	pass # Replace with function body.
