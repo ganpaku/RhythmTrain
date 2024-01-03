fc26489e7d04afa6ba9be0b148cb5ad7f3d4b687
ganpaku <fehrlejoni@gmail.com>
2024-01-03 19:14:27 +0100

    RetrowaveTerrain

---
+++ World/LaneScrollerPath.gd
@@ -0,0 +1,12 @@
+extends Path3D
+var lanes = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	lanes = get_children()
+	print(lanes)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	for child in lanes:
+		child.progress_ratio -= .5 * delta
